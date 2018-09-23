fc69fe1d4380f8ed10f641890c69b55ccbeac0c7
liev4die <alisasani@live.com>
2018-09-23 20:56:13 +0200

    klaar

---
+++ game.js
@@ -0,0 +1,140 @@
+// ali sasani
+// week 4 game
+
+
+
+
+//introduction
+alert("welcome to the quiz!")
+alert("There are 15 questions!")
+
+//question 1
+var question = prompt("1-How many Marvel movies are there?")
+if(question == "19"){
+	alert("Correct")
+	document.write("Question 1: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("Question 1: incorrent")
+	document.write("<br>Right answer: 19")
+}
+//question 2
+var question = prompt("2-In what year was the first Star Wars movie released?")
+if(question == "1977"){
+	alert("Correct")
+	document.write("<br><br>Question 2: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 2: incorrent")
+	document.write("<br>Right answer: 1977")
+}
+//question 3
+var question = prompt("3-For how many years was Steve Rogers frozen?")
+if(question == "70"){
+	alert("Correct")
+	document.write("<br><br>Question 3: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 3: incorrent")
+	document.write("<br>Right answer: 70")
+}
+//question 4
+var question = prompt("4-How many wishes did the genie grant Aladdin?")
+if(question == "3"){
+	alert("Correct")
+	document.write("<br><br>Question 4: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 4: incorrent")
+	document.write("<br>Right answer: 3")
+}
+//question 5
+var question = prompt("5-What's the name of the gigantic Australian shark in Finding Nemo?")
+if(question == "bruce"){
+	alert("Correct")
+	document.write("<br><br>Question 5: correct")
+}
+else if(question == "Bruce"){
+	alert("Correct")
+	document.write("<br><br>Question 5: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 5: incorrent")
+	document.write("<br>Right answer: Bruce")
+}
+//question 6
+var question = prompt("6-In The Emperor's New Groove, what animal is Emperor Kuzco turned into?")
+if(question == "llama"){
+	alert("Correct")
+	document.write("<br><br>Question 6: correct")
+}
+else if(question == "Llama"){
+	alert("Correct")
+	document.write("<br><br>Question 6: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 6: incorrent")
+	document.write("<br>Right answer: llama")
+}
+//question 7
+var question = prompt("7-What colour car is McQueen in Cars?")
+if(question == "red"){
+	alert("Correct")
+	document.write("<br><br>Question 7: correct")
+}
+else if(question == "Red"){
+	alert("Correct")
+	document.write("<br><br>Question 7: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 7: incorrent")
+	document.write("<br>Right answer: red")
+}
+//question 8
+var question = prompt("8-In what year was Counter Strike Global Offensive released?")
+if(question == "2012"){
+	alert("Correct")
+	document.write("<br><br>Question 8: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 8: incorrent")
+	document.write("<br>Right answer: 2012")
+}
+//question 9
+var question = prompt("9-What game was the first battle royale game?")
+if(question == "h1z1"){
+	alert("Correct")
+	document.write("<br><br>Question 9: correct")
+}
+else if(question == "H1Z1"){
+	alert("Correct")
+	document.write("<br><br>Question 9: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 9: incorrent")
+	document.write("<br>Right answer: h1z1")
+}
+//question 10
+var question = prompt("10-who is the biggest Youtuber?")
+if(question == "pewdiepie"){
+	alert("Correct")
+	document.write("<br><br>Question 10: correct")
+}
+else if(question == "Pewdiepie"){
+	alert("Correct")
+	document.write("<br><br<Question 10: correct")
+}
+else{
+	alert("Incorrent")
+	document.write("<br><br>Question 10: incorrent")
+	document.write("<br>Right answer: pewdiepie")
+}
